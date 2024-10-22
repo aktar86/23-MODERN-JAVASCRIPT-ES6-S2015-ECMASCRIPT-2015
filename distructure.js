b423185ba8fdeeca35d6dc4973f11032cc88c91b
aktar86 <amdaktar86@gmail.com>
2024-10-22 13:15:30 +0600

    added array distructure

--- distructure.js
+++ distructure.js
@@ -1,3 +1,11 @@
+//object distructure
 const person = {name : 'Tom', age: 20, phone: '01728456788', job: 'Student', friends: ['habib', 'sdik', 'emaa']};
 const {phone, job, friends} = person;
 console.log(phone, job, friends)
+
+
+
+//array distructure
+const friendList = ['kabir', 'jabir', 'sabir', 'dabir', 'rahim'];
+const [firstFriend, ...restFriends] = friendList;
+console.log(firstFriend, restFriends)
