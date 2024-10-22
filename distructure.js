7b7ace21228bddbf76b1af657a1da006511bab94
aktar86 <amdaktar86@gmail.com>
2024-10-22 13:07:39 +0600

    learned distructure in javascript

---
+++ distructure.js
@@ -0,0 +1,3 @@
+const person = {name : 'Tom', age: 20, phone: '01728456788', job: 'Student', friends: ['habib', 'sdik', 'emaa']};
+const {phone, job, friends} = person;
+console.log(phone, job, friends)
